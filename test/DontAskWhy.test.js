018c156cc2cecab0388f960009fe76c85752e714
Carmen V Ramos <carmenvramos@yahoo.com>
2018-07-13 11:02:25 -0700

    Intial commit

---
+++ test/DontAskWhy.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const { func arrays } = require('../lib/arrays');
+
+describe('Dont Ask Why', () => {
+    it('Takes array of words & returns a new array with words with y or Y removed', () => {
+        const array = ['sky', 'hot', 'Yeti', 'green'];
+        const newArray = dontaskwhy(array));
+        assert.equal(newArray, ['Is it hot?', 'Is it green?']);
+    
+});
