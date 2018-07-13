5ac922ff8565aba1f1070c5bc2f3fa2b0f33ecc8
Carmen V Ramos <carmenvramos@yahoo.com>
2018-07-13 11:13:02 -0700

    Fixed Travis

--- test/DontAskWhy.test.js
+++ test/DontAskWhy.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const { func arrays } = require('../lib/arrays');
+const { arrays } = require('../lib/arrays');
 
 describe('Dont Ask Why', () => {
     it('Takes array of words & returns a new array with words with y or Y removed', () => {
